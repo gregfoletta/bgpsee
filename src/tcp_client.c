ce439f635cfdfc00845a264c37677bb8f34a8c23
Greg Foletta <greg@foletta.org>
2023-08-15 08:25:46 +1000

    Moved headers out of tcp_client.h

--- src/tcp_client.c
+++ src/tcp_client.c
@@ -1,5 +1,12 @@
 #include "tcp_client.h"
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <strings.h> 
+#include <unistd.h>
+
+#include "debug.h"
 
 
 int tcp_connect(const char *host, const char *port) {
